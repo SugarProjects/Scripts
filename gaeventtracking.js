33bad0daf56c97d8cc2d3ce2276cdff9e03de5ae
Daniel Stenson <dstenson83@hotmail.co.uk>
2019-08-20 15:24:06 +0100

    Create gaeventtracking.js

---
+++ gaeventtracking.js
@@ -0,0 +1,45 @@
+// Google Analytics Event Tracking
+function ga_track_outbound_link(event) {
+    // Find parent link from possible child element
+    var href = event.target.closest('a');
+    if (!href) {
+        console.warn('Href cannot be null.');
+        return;
+    }
+    ga('send', {
+        hitType: 'event',
+        eventCategory: 'Outbound Link',
+        eventAction: 'click',
+        eventLabel: href
+    });
+}
+
+// Listen for events
+document.addEventListener('click', function (event) {
+    if (event.target.matches('a[href^="mailto:"]') // mailto function
+		|| event.target.matches('a[href^="tel:"]') // tel: function
+		|| event.target.matches('a[href$=".pdf"]') // pdf function
+    ) {
+	    console.log(event);
+	    ga_track_outbound_link(event);
+	    return true;
+    }
+}, false);
+
+// Polyfills
+if (!Element.prototype.matches) {
+  Element.prototype.matches = Element.prototype.msMatchesSelector || 
+                              Element.prototype.webkitMatchesSelector;
+}
+
+if (!Element.prototype.closest) {
+  Element.prototype.closest = function(s) {
+    var el = this;
+
+    do {
+      if (el.matches(s)) return el;
+      el = el.parentElement || el.parentNode;
+    } while (el !== null && el.nodeType === 1);
+    return null;
+  };
+}
